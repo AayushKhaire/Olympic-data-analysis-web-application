40e370f1a66c45c0307b41675288303d24c2874d
AayushKhaire <aayushkhaire4558@gmail.com>
2025-09-02 21:35:53 +0530

    processor.py

---
+++ processor.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def preprocess(df1,regions_df):
+
+    df1=df1[df1['Season']=='Summer']
+
+    df1=df1.merge(regions_df,on='NOC',how='left')
+
+    df1.drop_duplicates(inplace=True)
+
+    df1=pd.concat([df1,pd.get_dummies(df1['Medal'])],axis=1)
+
+    return df1
